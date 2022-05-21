0f7046fc21148aae35865bad35780915e4631789
terwer <youweics@163.com>
2022-05-22 00:22:32 +0800

    Add xfce4 tiger vncserver

---
+++ arch-linux-vnc/config/my_profile.sh
@@ -0,0 +1,16 @@
+# my profile
+
+# locals
+export LANGUAGE=en_US:en
+export LC_ALL=C
+export LC_CTYPE=zh_CN.UTF-8
+export LANG=en_US.UTF-8
+# echo "local setup finished"
+
+# alias
+alias ll='ls -l'
+alias la='ls -a'
+alias lla='ls -l -a'
+# echo "alias setup finished"
+
+echo "reset env"
