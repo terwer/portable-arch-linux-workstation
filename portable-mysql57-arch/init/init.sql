c8406f42a05361d05ce011b598f02a50e0a11e87
terwer <youweics@163.com>
2022-05-23 00:24:40 +0800

    fix:Fix connection issues

--- portable-mysql57-arch/init/init.sql
+++ portable-mysql57-arch/init/init.sql
@@ -1,3 +1,5 @@
 CREATE USER 'terwer'@'%' IDENTIFIED BY '123456';
 GRANT ALL PRIVILEGES ON *.* TO 'terwer'@'%' WITH GRANT OPTION;
 flush privileges;
+
+-- create database test default character set utf8mb4 collate utf8mb4_general_ci;
